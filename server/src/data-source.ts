6bc3f9dd538d6f266439dd544ded3e162c991708
Nikita <nikita455@gmail.com>
2022-09-05 09:53:25 +0300

    Setup GraphQL Server with Express and Apollo Server

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,17 +1,17 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
+  type: "postgres",
+  host: "localhost",
   port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'jwt_auth_qraphql',
+  username: "postgres",
+  password: "postgres",
+  database: "jwt_auth_qraphql",
   synchronize: true,
   logging: false,
   entities: [User],
   migrations: [],
   subscribers: [],
-})
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,47 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
 
-AppDataSource.initialize().then(async () => {
+(async () => {
+  const app = express();
+  app.get("/", (_req, res) => res.send("hello"));
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+  const apolloServer = new ApolloServer({
+    typeDefs: `
+      type Query {
+        hello: String!
+      }`,
+    resolvers: {
+      Query: {
+        hello: () => "hello world",
+      },
+    },
+  });
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
+  await apolloServer.start();
 
-    console.log("Here you can setup and run express / fastify / any other framework.")
+  apolloServer.applyMiddleware({ app });
 
-}).catch(error => console.log(error))
+  app.listen(4000, () => {
+    console.log("express server started");
+  });
+})();
+
+// AppDataSource.initialize()
+//   .then(async () => {
+//     console.log("Inserting a new user into the database...")
+//     const user = new User()
+//     user.firstName = "Timber"
+//     user.lastName = "Saw"
+//     user.age = 25
+//     await AppDataSource.manager.save(user)
+//     console.log("Saved a new user with id: " + user.id)
+
+//     console.log("Loading users from the database...")
+//     const users = await AppDataSource.manager.find(User)
+//     console.log("Loaded users: ", users)
+
+//     console.log(
+//       "Here you can setup and run express / fastify / any other framework."
+//     )
+//   })
+//   .catch(error => console.log(error))
