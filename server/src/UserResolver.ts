3ec9d06abc5a2b8b94166c7df4f953f1a47d0b7d
Nikita <nikita455@gmail.com>
2022-09-05 10:46:53 +0300

    Create Register mutation

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,9 +1,17 @@
-import { Resolver, Query } from "type-graphql";
+import { Resolver, Query, Mutation, Arg } from "type-graphql";
 
 @Resolver()
 export class UserResolver {
   @Query(() => String)
   hello() {
     return "hi";
+  }
+
+  @Mutation()
+  register(
+    @Arg("email", () => String) email: string,
+    @Arg("password", () => String) password: string
+  ) {
+    return;
   }
 }
