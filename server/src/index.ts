63c2ea7e23cd4ad1c1f7801cd286f0c547009d72
Nikita <nikita455@gmail.com>
2022-09-05 10:30:06 +0300

    Setup TypeGraphQL (resolvers and schemas)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,15 @@
 import express from "express";
+import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./UserResolver";
 
 (async () => {
   const app = express();
   app.get("/", (_req, res) => res.send("hello"));
 
   const apolloServer = new ApolloServer({
-    typeDefs: `
-      type Query {
-        hello: String!
-      }`,
-    resolvers: {
-      Query: {
-        hello: () => "hello world",
-      },
-    },
+    schema: await buildSchema({ resolvers: [UserResolver] }),
   });
 
   await apolloServer.start();
@@ -25,23 +20,3 @@ import { ApolloServer } from "apollo-server-express";
     console.log("express server started");
   });
 })();
-
-// AppDataSource.initialize()
-//   .then(async () => {
-//     console.log("Inserting a new user into the database...")
-//     const user = new User()
-//     user.firstName = "Timber"
-//     user.lastName = "Saw"
-//     user.age = 25
-//     await AppDataSource.manager.save(user)
-//     console.log("Saved a new user with id: " + user.id)
-
-//     console.log("Loading users from the database...")
-//     const users = await AppDataSource.manager.find(User)
-//     console.log("Loaded users: ", users)
-
-//     console.log(
-//       "Here you can setup and run express / fastify / any other framework."
-//     )
-//   })
-//   .catch(error => console.log(error))
