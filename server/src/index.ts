b397ee3fb165dfcd039bb22c018e46d50dbab47a
Nikita <nikita455@gmail.com>
2022-09-06 11:42:47 +0300

    Implement refresh token endpoint and verify refresh token and return new access token if needed

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,10 +6,39 @@ import { buildSchema } from "type-graphql";
 import { UserResolver } from "./UserResolver";
 import { AppDataSource } from "./data-source";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import cookieParser from "cookie-parser";
+import { verify } from "jsonwebtoken";
+import { User } from "./entity/User";
+import { createAccessToken } from "./auth";
 
 (async () => {
   const app = express();
+  app.use(cookieParser());
   app.get("/", (_req, res) => res.send("hello"));
+  app.post("/refresh_token", async (req, res) => {
+    const token = req.cookies.jid;
+    if (!token) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    let payload: any = null;
+    try {
+      payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
+    } catch (err) {
+      console.log(err);
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    // token is valid and
+    // we can send back access token
+    const user = await User.findOne({ where: { id: payload.userId } });
+
+    if (!user) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    return res.send({ ok: true, accessToken: createAccessToken(user) });
+  });
 
   await AppDataSource.initialize();
 
