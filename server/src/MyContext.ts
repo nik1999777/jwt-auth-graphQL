0a895af42919e4c7b736b226f108738176299071
Nikita <nikita455@gmail.com>
2022-09-06 11:01:40 +0300

    Implement protected routes with resolver middleware (verify access token)

--- server/src/MyContext.ts
+++ server/src/MyContext.ts
@@ -3,4 +3,5 @@ import { Response, Request } from "express";
 export interface MyContext {
   req: Request;
   res: Response;
+  payload?: { userId: string };
 }
--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -6,11 +6,13 @@ import {
   Field,
   ObjectType,
   Ctx,
+  UseMiddleware,
 } from "type-graphql";
 import { compare } from "bcryptjs";
 import { User } from "./entity/User";
 import { MyContext } from "./MyContext";
 import { createAccessToken, createRefreshToken } from "./auth";
+import { isAuth } from "./isAuth";
 
 @ObjectType()
 class LoginResponse {
@@ -22,7 +24,14 @@ class LoginResponse {
 export class UserResolver {
   @Query(() => String)
   hello() {
-    return "hi";
+    return "hi!";
+  }
+
+  @Query(() => String)
+  @UseMiddleware(isAuth)
+  bye(@Ctx() { payload }: MyContext) {
+    console.log(payload);
+    return `your user id is: ${payload!.userId}`;
   }
 
   @Query(() => [User])
